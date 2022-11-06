2699adf50d28a4a122eb96b75af05fc75857292a
coleyoung02 <coleyoung741@gmail.com>
2022-11-06 00:19:41 -0700

    nicer healthbar
    healthbar looks more polished

--- YakamotoCleanSweep/Assets/Scripts/health/healthbar.cs
+++ YakamotoCleanSweep/Assets/Scripts/health/healthbar.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class healthbar : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private Image foreground;
-    private Canvas canvas;
+    [SerializeField] private Slider foreground;
+    [SerializeField] private Image fill;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private Gradient grad;
     private Camera cam;
 
     private void Start()
@@ -18,10 +20,11 @@ public class healthbar : MonoBehaviour
 
     private void Update()
     {
-        foreground.GetComponentInParent<Canvas>().transform.rotation = Quaternion.LookRotation(foreground.transform.position - cam.transform.position);
+        canvas.transform.rotation = Quaternion.LookRotation(foreground.transform.position - cam.transform.position);
     }
     
     public void sethealth(int maxhealth, int health){
-        foreground.fillAmount = ((float)maxhealth-(float)health)/(float)maxhealth;
+        fill.color = grad.Evaluate(((float)maxhealth-(float)health)/(float)maxhealth);
+        foreground.value = ((float)maxhealth-(float)health)/(float)maxhealth;
     }
 }
