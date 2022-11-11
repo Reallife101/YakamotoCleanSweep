47dd15bf5219532634f4cc79f1615ed7105fc364
LeoParagili <paragili02@gmail.com>
2022-11-10 16:41:31 -0800

    Movement Rework
    Currently reworking movement to utilize an input manager and cinemachine, in the hopes of polishing the system and making it smoother.

---
+++ YakamotoCleanSweep/Assets/Scripts/Leo_Movement/Movement 2.0/InputManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager _instance;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public Vector2 GetPlayerMovement()
+    {
+        return playerControls.Player.Movement.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetMouseDelta()
+    {
+        return playerControls.Player.Look.ReadValue<Vector2>();
+    }
+
+    public bool PlayerJumpedThisFrame()
+    {
+        return playerControls.Player.Jump.triggered;
+    }
+}
