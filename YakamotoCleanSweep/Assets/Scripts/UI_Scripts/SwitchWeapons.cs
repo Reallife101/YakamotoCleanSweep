c9776e90a02b4eb09967a4c0f9c99f055e0e1d0a
ringo23333 <ringo.pierzynski@gmail.com>
2022-11-21 02:00:27 -0800

    Weapon ui fix
    Got rid of updateUI script and moved it to WeaponSwap to make it work with the scroll wheel too

--- YakamotoCleanSweep/Assets/Scripts/UI_Scripts/SwitchWeapons.cs
+++ YakamotoCleanSweep/Assets/Scripts/UI_Scripts/SwitchWeapons.cs
@@ -7,6 +7,12 @@ public class SwitchWeapons : MonoBehaviour
     [SerializeField] private KeyCode[] swapKeys;
     [SerializeField] private GameObject[] weapons;
     [SerializeField] private int startingWeaponIndex = 0;
+    [SerializeField] GameObject sprayIcon;
+    [SerializeField] GameObject broomIcon;
+    [SerializeField] GameObject soapIcon;
+    [SerializeField] GameObject sprayCrosshair;
+    [SerializeField] GameObject broomCrosshair;
+    [SerializeField] GameObject soapCrosshair;
 
     // Start is called before the first frame update
     void Start()
@@ -68,6 +74,29 @@ public class SwitchWeapons : MonoBehaviour
             if (weapon == weapons[index])
             {
                 weapon.SetActive(true);
+
+                if (weapon.name == "Aerosol") {
+                    sprayIcon.SetActive(true);
+                    broomIcon.SetActive(false);
+                    soapIcon.SetActive(false);
+                    sprayCrosshair.SetActive(true);
+                    broomCrosshair.SetActive(false);
+                    soapCrosshair.SetActive(false);
+                } else if (weapon.name == "Broom") {
+                    sprayIcon.SetActive(false);
+                    broomIcon.SetActive(true);
+                    soapIcon.SetActive(false);
+                    sprayCrosshair.SetActive(false);
+                    broomCrosshair.SetActive(true);
+                    soapCrosshair.SetActive(false);
+                } else if (weapon.name == "SoapTalisman") {
+                    sprayIcon.SetActive(false);
+                    broomIcon.SetActive(false);
+                    soapIcon.SetActive(true);
+                    sprayCrosshair.SetActive(false);
+                    broomCrosshair.SetActive(false);
+                    soapCrosshair.SetActive(true);
+                }
             }
             else
             {
